1c15751e49247e7d35971862bfbc7d885cc19e2d
astiyanurha <astiyanurhayati18@gmail.com>
2025-08-10 22:48:29 +0700

    update malam senin

--- script.js
+++ script.js
@@ -1,40 +1,46 @@
- const navbar = document.getElementById('navbar');
+const navbar = document.getElementById('navbar');
 
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > 10) {
-      navbar.classList.add('bg-white/10', 'backdrop-blur-md');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 10) {
+    navbar.classList.add('bg-white/10', 'backdrop-blur-md');
+  } else {
+    navbar.classList.remove('bg-white/10', 'backdrop-blur-md');
+  }
+});
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const showMoreBtn = document.getElementById("show-more");
+  const cards = document.querySelectorAll(".card-ar");
+
+  showMoreBtn.addEventListener("click", () => {
+    cards.forEach((card, index) => {
+      // index mulai dari 0, jadi > 2 artinya mulai dari Card ke-4
+      if (index > 2) {
+        card.classList.toggle("hidden");
+      }
+    });
+
+
+
+   
+
+    // Toggle teks tombol
+    if (showMoreBtn.textContent === "Show more") {
+      showMoreBtn.textContent = "Show less";
     } else {
-      navbar.classList.remove('bg-white/10', 'backdrop-blur-md');
+      showMoreBtn.textContent = "Show more";
     }
   });
 
-
-const openBtn = document.getElementById('openModal');
-  const modal = document.getElementById('videoModal');
-  const closeBtn = document.getElementById('closeModal');
-  const iframe = document.getElementById('youtubePlayer');
-  const videoURL = "https://www.youtube.com/embed/dX-5u-gNuHo?autoplay=1";
-
-  openBtn.addEventListener('click', () => {
-    iframe.src = videoURL;
-    modal.classList.remove('hidden');
-    modal.classList.add('flex');
-  });
-
-  closeBtn.addEventListener('click', () => {
-    iframe.src = "";
-    modal.classList.add('hidden');
-    modal.classList.remove('flex');
-  });
-
-  // Close modal if clicked outside iframe
-  modal.addEventListener('click', (e) => {
-    if (e.target === modal) {
-      iframe.src = "";
-      modal.classList.add('hidden');
-      modal.classList.remove('flex');
-    }
-  });
-
+  // Pas awal load, pastikan di mobile yang di-hide cuma setelah index 2
+  if (window.innerWidth < 768) {
+    cards.forEach((card, index) => {
+      if (index > 2) card.classList.add("hidden");
+    });
+  }
+});
 
 
